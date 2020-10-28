b8c8a9df7164fab50cafbabbff88d0d27df7af9f
Git-kun <56929408+Git-kun@users.noreply.github.com>
2020-10-28 13:20:44 +0900

    My Todos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 (function() {
   'use strict'; //エラーチェック
+
+  var vm = new Vue({
+    el: '#app',
+    data: {
+      todos: [
+        'task 1',
+        'task 2',
+        'task 3'
+      ]
+
+    }
+  });
 })();
